dfd5a2862f30a538b2ee3c865fd9e9f2e21b774d
BangLV <wuubangdev@gmail.com>
2024-09-12 08:28:26 +0700

    change icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <Head>
-        <link rel="icon" href="/images/logo_png.png" type="image/png" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap&subset=vietnamese"
           rel="stylesheet"
